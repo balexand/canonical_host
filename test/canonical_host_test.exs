357a304b1c83bba85bd804b64f93364328e257c4
Brian Alexander <balexand@gmail.com>
2022-10-20 13:58:05 -0600

    implement (#2)
    * implement
    
    * Delete config.exs
    
    * deps

--- test/canonical_host_test.exs
+++ test/canonical_host_test.exs
@@ -1,8 +1,39 @@
 defmodule CanonicalHostTest do
-  use ExUnit.Case
-  doctest CanonicalHost
+  use ExUnit.Case, async: true
+  use Plug.Test
+  import Phoenix.ConnTest, only: [redirected_to: 1]
 
-  test "greets the world" do
-    assert CanonicalHost.hello() == :world
+  test "host matches canonical host doesn't redirect" do
+    conn = conn(:get, "/") |> CanonicalHost.call(CanonicalHost.init("https://www.example.com/"))
+
+    refute conn.halted
+  end
+
+  test "POST doesn't redirect" do
+    conn = conn(:post, "/") |> CanonicalHost.call(CanonicalHost.init("http://canonical.com"))
+
+    refute conn.halted
+  end
+
+  test "disabled when passed nil" do
+    conn = conn(:get, "/") |> CanonicalHost.call(CanonicalHost.init(nil))
+
+    refute conn.halted
+  end
+
+  test "redirects to http with no query string" do
+    conn = conn(:get, "/") |> CanonicalHost.call(CanonicalHost.init("http://canonical.com"))
+
+    assert conn.halted
+    assert redirected_to(conn) == "http://canonical.com/"
+  end
+
+  test "redirects to https with query string" do
+    conn =
+      conn(:get, "/my/path?a=b&c=d")
+      |> CanonicalHost.call(CanonicalHost.init("https://canonical.com"))
+
+    assert conn.halted
+    assert redirected_to(conn) == "https://canonical.com/my/path?a=b&c=d"
   end
 end
