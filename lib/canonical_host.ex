5a9f3b59ab6478c7fceefc9584768e10c5d24ab2
Brian Alexander <balexand@gmail.com>
2022-10-20 13:50:34 -0600

    implement

--- lib/canonical_host.ex
+++ lib/canonical_host.ex
@@ -1,18 +1,28 @@
 defmodule CanonicalHost do
-  @moduledoc """
-  Documentation for `CanonicalHost`.
-  """
+  @behaviour Plug
 
-  @doc """
-  Hello world.
+  @impl true
+  def init(nil), do: %{host: nil, scheme: nil}
 
-  ## Examples
+  def init(canonical_scheme_and_host) do
+    uri = URI.parse(canonical_scheme_and_host)
 
-      iex> CanonicalHost.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    %{host: uri.host, scheme: uri.scheme}
   end
+
+  @impl true
+  def call(conn, %{host: nil}), do: conn
+  def call(%Plug.Conn{host: host} = conn, %{host: host}), do: conn
+  def call(%Plug.Conn{method: method} = conn, %{}) when method != "GET", do: conn
+
+  def call(conn, %{host: host, scheme: scheme}) do
+    location = "#{scheme}://#{host}#{conn.request_path}#{query_suffix(conn.query_string)}"
+
+    conn
+    |> Phoenix.Controller.redirect(external: location)
+    |> Plug.Conn.halt()
+  end
+
+  defp query_suffix(""), do: ""
+  defp query_suffix(query_string), do: "?#{query_string}"
 end
