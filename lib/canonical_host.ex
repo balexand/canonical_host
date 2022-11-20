065086ca8573e0778820833425a0591b1709c20b
Brian Alexander <balexand@gmail.com>
2022-11-20 15:57:37 -0700

    `:additional_hosts` option (#5)
    * Update canonical_host.ex
    
    * `:additional_hosts` option

--- lib/canonical_host.ex
+++ lib/canonical_host.ex
@@ -9,25 +9,35 @@ defmodule CanonicalHost do
   end
 
   @impl true
-
   def call(%Plug.Conn{method: "GET"} = conn, config_key: config_key) do
     case Application.get_env(:canonical_host, config_key) do
-      nil -> conn
-      opts -> do_call(conn, Keyword.get(opts, :host), Keyword.get(opts, :scheme, "https"))
+      nil ->
+        conn
+
+      opts ->
+        opts = Keyword.validate!(opts, [:additional_hosts, :host, :scheme])
+        do_call(conn, Keyword.get(opts, :host), opts)
     end
   end
 
   def call(conn, _), do: conn
 
-  defp do_call(conn, nil = _host, _scheme), do: conn
-  defp do_call(%Plug.Conn{host: host} = conn, host, _scheme), do: conn
+  defp do_call(conn, nil = _host, _opts), do: conn
+  defp do_call(%Plug.Conn{host: host} = conn, host, _opts), do: conn
 
-  defp do_call(conn, host, scheme) do
-    location = "#{scheme}://#{host}#{conn.request_path}#{query_suffix(conn.query_string)}"
+  defp do_call(conn, host, opts) do
+    additional_hosts = Keyword.get(opts, :additional_hosts, [])
+    scheme = Keyword.get(opts, :scheme, "https")
 
-    conn
-    |> Phoenix.Controller.redirect(external: location)
-    |> Plug.Conn.halt()
+    if conn.host in additional_hosts do
+      conn
+    else
+      location = "#{scheme}://#{host}#{conn.request_path}#{query_suffix(conn.query_string)}"
+
+      conn
+      |> Phoenix.Controller.redirect(external: location)
+      |> Plug.Conn.halt()
+    end
   end
 
   defp query_suffix(""), do: ""
